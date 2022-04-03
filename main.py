ee2953d31954a0325f150cacf9497a41705cd7ec
Kentaro Kawano <kentaro.kawano@gmail.com>
2022-04-03 23:37:56 +0900

    メッセージの受信まで

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import functions_framework
 import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+# process_before_response must be True when running on FaaS
+app = App(process_before_response=True)
+handler = SlackRequestHandler(app)
+
+
+@app.event("message")
+def message_channel(body, say, logger):
+    logger.info(body)
 
 
 @functions_framework.http
-def verify(request):
-    headers = {"Content-Type": "application/json"}
-    body = request.get_json()
-    if body.get("type") == "url_verification":
-        res = json.dumps({"challenge": body["challenge"]})
-        return (res, 200, headers)
-
-    return ("{}", 400, headers)
+def echo_bot(request):
+    return handler.handle(request)
---
+++ script/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+gcloud functions deploy echo-bot --entry-point echo_bot \
+  --trigger-http --allow-unauthenticated \
+  --env-vars-file .env.yaml
+
