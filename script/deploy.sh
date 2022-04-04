87980e77aeac41557d19a67c8fbf88314e26afe0
Kentaro Kawano <kentaro.kawano@gmail.com>
2022-04-04 20:44:22 +0900

    余計な空行を削除

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
 gcloud functions deploy echo-bot --entry-point echo_bot \
   --trigger-http --allow-unauthenticated \
   --env-vars-file .env.yaml
-
--- script/deploy_verification_script.sh
+++ script/deploy_verification_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 gcloud functions deploy echo-bot --entry-point verify \
   --runtime python39 \
   --trigger-http --allow-unauthenticated
